efae9f44da1c46997f0c4f1bf5d6c04d81fb0b57
rachidKamili <rachidkamili.pro@gmail.com>
2018-04-04 23:47:33 -0400

    Add listener to "Add" button

--- app/src/main/java/mel/kamili/rachid/carsapp/fragments/AddCarFragment.java
+++ app/src/main/java/mel/kamili/rachid/carsapp/fragments/AddCarFragment.java
@@ -6,18 +6,22 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import mel.kamili.rachid.carsapp.R;
+import mel.kamili.rachid.carsapp.model.Car;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class AddCarFragment extends Fragment {
+public class AddCarFragment extends Fragment implements View.OnClickListener {
 
     private EditText etModel;
     private EditText etType;
     private EditText etYear;
+    private Button addBtn;
 
     public AddCarFragment() {
         // Required empty public constructor
@@ -37,6 +41,15 @@ public class AddCarFragment extends Fragment {
         etModel = v.findViewById(R.id.etModel);
         etType = v.findViewById(R.id.etType);
         etYear = v.findViewById(R.id.etYear);
+        addBtn = v.findViewById(R.id.addCarBtn);
+        addBtn.setOnClickListener(this);
     }
 
+    @Override
+    public void onClick(View v) {
+        Car car = new Car(etModel.getText().toString(),
+                etType.getText().toString(),
+                etYear.getText().toString());
+        Toast.makeText(getContext(), car.getModel(), Toast.LENGTH_SHORT).show();
+    }
 }
