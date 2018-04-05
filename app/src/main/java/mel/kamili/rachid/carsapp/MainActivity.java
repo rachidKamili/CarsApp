37f44e88a28010690abb464094f91c1f0e45a322
rachidKamili <rachidkamili.pro@gmail.com>
2018-04-04 23:26:09 -0400

    Adding car list fragment to the activity

--- app/src/main/java/mel/kamili/rachid/carsapp/MainActivity.java
+++ app/src/main/java/mel/kamili/rachid/carsapp/MainActivity.java
@@ -3,11 +3,24 @@ package mel.kamili.rachid.carsapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import mel.kamili.rachid.carsapp.fragments.CarsFragment;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String CARS_FRAG_TAG = "CARS_FRAG_TAG";
+    private CarsFragment carsFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        carsFragment = new CarsFragment();
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.flCarsHolder, carsFragment, CARS_FRAG_TAG)
+                .addToBackStack(null)
+                .commit();
+
     }
 }
